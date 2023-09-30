5cb7a6f12b7701ea18c55b02a0dc305d50f165a9
Tayyaba Shahid <136459395+TAYYABASHAHID1@users.noreply.github.com>
2023-09-30 17:46:00 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+var age = 30; // Replace 30 with your actual age
+alert("My age is: " + age);
+
+
+
+
+var  numberofvisits= 7+7 
+
+alert("you have visited this site " + numberofvisits,"time");
+
+
+
+
+var birthYear = 2010; 
+alert("My birth year is: " + birthYear);
+
+
+
+
+
+
+
+
+
+
+
+var visitorName = "John Doe";
+var productTitle = "T-shirt";
+var quantity = 5;
+
+var message = visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store.";
+
+
+document.write(message);
